91fa7b87984d03526bdd89bda7aeee10cd880a48
samuel <samuel.bae@skola.taby.se>
2025-11-06 13:14:07 +0100

    Update quiz.py
    Loop med flera försök

--- quiz.py
+++ quiz.py
@@ -1,11 +1,15 @@
-#1-hälsning
 namn=input("Hej vad heter du?")
 print(f"Hej {namn} och vällkommen till spelet")
 
 import random
-hemligt_tal= random.randint(1,10)
-gissning=int(input("Gissa ett tal mellan 1 och 10:"))
-if gissning==hemligt_tal:
-    print("Din gissning är rätt")
+hemligt_tal1 = random.randint(1, 10)
+for försök in range(1,4):
+    gissnings_tal = int(input(f"Försök {försök}: Gissa ett tal mellan 1 och 10: "))
+    
+    if gissnings_tal == hemligt_tal1:
+        print("Din gissning är rätt!")
+        break
+    else:
+        print("Det är fel")
 else:
-    print(f"tyvärr, det var {hemligt_tal}")
+    print(f"Tyvärr, det var {hemligt_tal1}")
