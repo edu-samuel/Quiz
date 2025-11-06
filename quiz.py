4cf1618e5691bc026640de203ae7973b103285ce
samuel <samuel.bae@skola.taby.se>
2025-11-06 12:39:01 +0100

    Update quiz.py
    Slumpat tal och gissning

--- quiz.py
+++ quiz.py
@@ -1,3 +1,11 @@
 #1-hälsning
 namn=input("Hej vad heter du?")
 print(f"Hej {namn} och vällkommen till spelet")
+
+import random
+hemligt_tal= random.randint(1,10)
+gissning=int(input("Gissa ett tal mellan 1 och 10:"))
+if gissning==hemligt_tal:
+    print("Din gissning är rätt")
+else:
+    print(f"tyvärr, det var {hemligt_tal}")
